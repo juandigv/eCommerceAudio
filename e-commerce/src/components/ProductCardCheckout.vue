b7c67cd892f24a1676ffa3cf24bcfa1b390d4104
Juan Garcia <juandigv69@gmail.com>
2021-06-29 20:43:51 -0400

    Implemented ProductCard for Checkout Page

---
+++ e-commerce/src/components/ProductCardCheckout.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card
+    class="product-card"
+    width="286"
+    height="500"
+    color="var(--v-charcoal50-base)"
+  >
+    <div class="product-info-card">
+      <v-card
+        class="product-info-card"
+        flat
+        tile
+        :color="getColor(item.infoColor)"
+        v-if="item.info != ''"
+      >
+        <div class="product-info-text">{{ item.info }}</div>
+      </v-card>
+    </div>
+    <div class="product-name-text">{{ item.name }}</div>
+    <br />
+    <div class="product-price-text">${{ item.price }}</div>
+
+    <v-img class="product-image" :aspect-ratio="1 / 1" :src="item.src"></v-img>
+    <v-btn class="product-button" rounded color="var(--v-primary500-base)"> BUY </v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ProductCardCheckout",
+  props: {
+    item: Object,
+  },
+  data() {
+    return {};
+  },
+  components: {},
+  computed: {},
+  methods: {
+    getColor(type) {
+      switch (type) {
+        case "1":
+          return this.$vuetify.theme.themes.light.primary500;
+        case "2":
+          return this.$vuetify.theme.themes.light.success500;
+        case "3":
+          return this.$vuetify.theme.themes.light.error500;
+        case "4":
+          return this.$vuetify.theme.themes.light.info500;
+      }
+    },
+  },
+};
+</script>
+<style src="./productCardCheckout.css" scoped></style>
---
+++ e-commerce/src/views/Checkout.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="main-content">
+    <v-row justify="space-around" no-gutters>
+      <v-col style="max-width:min-content;max-height:min-content;display:flex;">
+        <ProductCardCheckout
+        :item="extra[0]"
+      /></v-col>
+       <v-col style="max-width:min-content;max-height:min-content;display:flex;">
+        <ProductCardCheckout
+        :item="extra[1]"
+      /></v-col>
+    </v-row>
+    
+  </div>
+</template>
+
+<script>
+import ProductCardCheckout from "../components/ProductCardCheckout";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      extra: [
+        {
+          id: 1,
+          src: "https://pontreyes.com/1243-large_default/auriculares-sennheiser-hd600.jpg",
+          name: "Sennheiser HD600",
+          detail: "Requires external DAC",
+          price: "379.99",
+          infoColor: "3",
+          info: "Low Stock",
+        },
+         {
+          id: 2,
+          src: "https://switch.com.my/wp-content/uploads/2020/05/Soundlink-Micro-Orange-1.png",
+          name: "SoundLink Micro Bluetooth® speaker",
+          detail: "Requires external DAC",
+          price: "99.00",
+          infoColor: "2",
+          info: "Special offer",
+        },
+      ],
+    };
+  },
+  components: {
+    ProductCardCheckout,
+  },
+};
+</script>
+
+<style scoped>
+.main-content {
+  max-width: 1280px;
+  width: 100%;
+  margin: 0 auto;
+}
+</style>

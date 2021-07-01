0415901401be7be8b353fb56d4c0733d4c0a8f38
Juan Garcia <juandigv69@gmail.com>
2021-06-30 23:39:54 -0400

    Implemented initial account page

--- e-commerce/src/router/index.js
+++ e-commerce/src/router/index.js
@@ -28,6 +28,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Checkout.vue"),
   },
+  {
+    path: "/Account",
+    name: "Account",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Account.vue"),
+  },
 ];
 
 const router = new VueRouter({
---
+++ e-commerce/src/views/Account.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main-content">
+    <v-row justify="space-between" no-gutters>
+      <v-col>
+        <AccountForm />
+      </v-col>
+      <v-col>
+         <h1>Blank</h1>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import AccountForm from "@/components/AccountForm.vue";
+
+export default {
+  name: "Home",
+  components: {
+    AccountForm,
+  },
+};
+</script>
+
+<style scoped>
+.main-content {
+  max-width: 1280px;
+  width: 100%;
+  margin: 0 auto;
+}
+</style>

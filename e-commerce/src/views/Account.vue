3b20dfb57ee4174c2178734e721a49d31d038740
Rodrigo Heredia <rodrigoh1205@gmail.com>
2021-06-30 23:48:21 -0400

    Adding bannerRegister to Account view

--- e-commerce/src/views/Account.vue
+++ e-commerce/src/views/Account.vue
@@ -5,7 +5,7 @@
         <AccountForm />
       </v-col>
       <v-col>
-         <h1>Blank</h1>
+        <BannerRegister :detailBanner="bannerReg" />
       </v-col>
     </v-row>
   </div>
@@ -13,11 +13,24 @@
 
 <script>
 import AccountForm from "@/components/AccountForm.vue";
+import BannerRegister from "@/components/BannerRegister.vue";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      bannerReg: {
+        img: "https://d2rbodpj0xodc.cloudfront.net/stories/1441380214247523754/bad03d87-9aae-45ac-8362-648c3de21e77.jpeg",
+        detail: "Sign up",
+        detail2: "Exclusive offers",
+        detail3: "Exclusive product",
+        detail4: "Exclusive clients",
+      },
+    };
+  },
   components: {
     AccountForm,
+    BannerRegister,
   },
 };
 </script>

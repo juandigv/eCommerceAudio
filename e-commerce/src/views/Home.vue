854c71c9126e977c5668d0a0d65be7ff431e09ff
Juan Garcia <juandigv69@gmail.com>
2021-06-30 21:58:26 -0400

    Fixed issues with styles from productCard with certain browsers

--- e-commerce/src/views/Home.vue
+++ e-commerce/src/views/Home.vue
@@ -83,7 +83,7 @@ export default {
           detail: "Price depends on color",
           price: "17.99",
           infoColor: "4",
-          info: "Coupons Available",
+          info: "PreOrder Available",
         },
       ],
       items1: [
